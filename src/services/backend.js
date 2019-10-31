dc3d3365d9c6c70eeac9404612a5d0015e86da9b
github username <github email address>
2019-10-31 17:52:32 -0400

    g

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,6 +1,6 @@
 import queryString from 'query-string';
 
-export const appUrl = "https://current-input-back.herokuapp.com/"
+export const appUrl = "https://current-input-back.herokuapp.com/api/v1/"
 // export const appUrl= "http://localhost:3000/api/v1/"
 
 
