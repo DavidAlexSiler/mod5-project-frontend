a5f9f8c4ae6ca2e3a0bf1a8cc0bc16de1a2b672d
github username <github email address>
2019-10-31 15:14:47 -0400

    removed songsearcher for research

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,9 +1,9 @@
 import loginReducer from './login';
-import songsearchReducer from './songsearch';
+// import songsearchReducer from './songsearch';
 import playlistReducer from './playlist';
 import friendsReducer from './friends';
 import { combineReducers } from 'redux';
-
+// console.log(songsearchReducer, 'song  search')
 export default combineReducers({
     login: loginReducer,
     songsearch: songsearchReducer,
