0048d6952d8f0302d1148a21c5fa4203346a4edc
github username <github email address>
2019-11-04 14:04:47 -0500

    heroku day 2

--- src/services/backend.js
+++ src/services/backend.js
@@ -4,16 +4,16 @@ export const appUrl = "https://shocking-witch-74494.herokuapp.com/api/v1/"
 // export const appUrl= "http://localhost:3000/api/v1/"
 
 
-export function getUser(){
+export async function getUser(){
     let parsed = queryString.parse(window.location.search)
-    return fetch(`${appUrl}users`, {
+    const r = await fetch(`${appUrl}users`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(parsed)
-    })
-    .then(r => r.json());
+    });
+    return await r.json();
 }
 
 
