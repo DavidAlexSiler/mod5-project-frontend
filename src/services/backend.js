c7d1416fc67ba9f43c458085365f236b67d4ab81
github username <github email address>
2019-11-04 14:11:03 -0500

    removed asynch in services

--- src/services/backend.js
+++ src/services/backend.js
@@ -4,16 +4,16 @@ export const appUrl = "https://shocking-witch-74494.herokuapp.com/api/v1/"
 // export const appUrl= "http://localhost:3000/api/v1/"
 
 
-export async function getUser(){
+export function getUser(){
     let parsed = queryString.parse(window.location.search)
-    const r = await fetch(`${appUrl}users`, {
+    return fetch(`${appUrl}users`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(parsed)
-    });
-    return await r.json();
+    })
+    .then(r => r.json());
 }
 
 
